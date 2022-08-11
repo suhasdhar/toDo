bace17eb4298cb84d62cbcc05578afe6adeadc45
Suhas Dhar <suhasdhar@gmail.com>
2022-08-10 19:58:06 -0500

    more boiler plate

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ const bodyParser= require("body-parser");
 const app=express();
 
 app.get("/",function(req,res){
-
-    res.send("hello");
+    res.sendFile(__dirname + "/index.html");
 })
 app.listen(3000,function(){
     console.log("server started on port 3000")
